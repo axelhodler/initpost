dadcead3187c996c01612852f3346144764c25a8
Axel Hodler <axel@hodler.co>
2018-08-04 08:38:21 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+
+Should generate an initial file close to
+
+```
+---
+--layout: post
+--title: "Graceful Exceptions"
+--date:   2018-07-27 13:32:17
+--description: ""
+--category:
+--tags: []
+---
+```
---
+++ initpost.hs
@@ -0,0 +1,30 @@
+module InitPost where
+
+import System.Environment
+import Data.Time.Clock
+import Data.Time.Calendar
+
+main :: IO ()
+main = do
+  args <- getArgs
+  day <- utctDay <$> getCurrentTime
+  writeFile "2018-08-03-busconf.md" $ unlines [
+    "---",
+    "layout: post",
+    "title: \"" ++ head args ++ "\"",
+    "date: " ++ show day,
+    "description: \"\"",
+    "category:",
+    "tags: []",
+    "---",
+    "",
+    "Content goes here"]
+
+---
+--layout: post
+--title: "Graceful Exceptions"
+--date:   2018-07-27 13:32:17
+--description: ""
+--category:
+--tags: []
+-----
